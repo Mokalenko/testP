aaeb7a4012e428123478a192ad9b1f4d1117b942
Maks <moskalenko.maksim1990@gmail.com>
2022-04-15 12:53:39 +0300

    les 19-21

--- js/script19Практика.js
+++ js/script19Практика.js
@@ -1,7 +1,7 @@
 "use strict"; // директива определяющая что код работает в новом варианте кода 
 //(строгий режим задания переменных)
 const numberOfFilms = +prompt("Сколько фильмов Вы пострели","5");
-    // 19 урок
+//!!! 19 урок
 //alert(numberOfFilms);
 
 // const personalMovieDB = {
@@ -25,39 +25,90 @@ const numberOfFilms = +prompt("Сколько фильмов Вы пострел
 
 // console.log(personalMovieDB);
 
-// 20 урок
+//!!! 20 урок
 
-if (4==9) {
-    console.log('error');
+// if (4==9) {
+//     console.log('error');
+// }else{
+//     console.log('Ok');
+// };
+
+// const num = 145;
+
+// if (num < 49) {
+//     console.log('error');
+// }else if (num > 100){ 
+//     console.log('error2');
+// }else{
+//     console.log('OK');
+// };
+
+// (num ===145) ? console.log('OK') : console.log('error'); //пряма запись иф елс
+
+// const num = 34;
+// //switch - строгое сравнение
+// switch (num) {
+//     case 49:
+//         console.log('Не верно');
+//         break;
+//     case 45:
+//         console.log('OK');
+//         break;
+//     case 100:
+//         console.log('Не верно');
+//         break;
+//     default:
+//         console.log('Ничего не подошло');
+//         break;
+// };
+
+//!!! 21 урок
+
+// const hamburger = true;
+// const fries = true;
+
+// if (hamburger && fries){
+// console.log('Ya poel');
+// };
+
+// console.log((hamburger && fries));
+
+// const hamburger = 5;
+// const fries = 0;
+
+// if (hamburger && fries){
+// console.log('Ya poel');
+// }else{
+//     console.log('Ne poel');
+// }
+
+// console.log((hamburger && fries));
+
+// const hamburger = 4;
+// const fries = 1;
+// const cola = 5;
+// // if (hamburger===3 && fries===1){
+//     // проверка числа на булево - просто налчие значение - тру, 0 - фолс
+// if (hamburger >=3 && fries && cola){
+//     console.log('Ya poel');
+// }else{
+//     console.log('Ne poel');
+// }
+// console.log((hamburger >=3 && fries && cola)); // вертает первое ложное значение
+// или последнее трушное
+// console.log((hamburger >=3 && '' && cola));
+
+const hamburger = 4;
+const fries = 1;
+const cola = 0;
+const nuggets = 2;
+
+if (hamburger >=3 && !cola || fries >1 && nuggets){
+    console.log('Ya poel');
 }else{
-    console.log('Ok');
-};
+    console.log('Ne poel');
+}
 
-const num = 145;
+let jonReport, samReport, marinaReport = 'done';
 
-if (num < 49) {
-    console.log('error');
-}else if (num > 100){ 
-    console.log('error2');
-}else{
-    console.log('OK');
-};
-
-(num ===145) ? console.log('OK') : console.log('error'); //пряма запись иф елс
-
-const num = 34;
-//switch - строгое сравнение
-switch (num) {
-    case 49:
-        console.log('Не верно');
-        break;
-    case 45:
-        console.log('OK');
-        break;
-    case 100:
-        console.log('Не верно');
-        break;
-    default:
-        console.log('Ничего не подошло');
-        break;
-};
+console.log(jonReport || samReport || marinaReport);
