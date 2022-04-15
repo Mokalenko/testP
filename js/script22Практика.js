60f678c25f4e1af5cfe20e17d28362260efd9fda
Maks <moskalenko.maksim1990@gmail.com>
2022-04-15 15:33:12 +0300

    praktika ciklu

--- js/script22Практика.js
+++ js/script22Практика.js
@@ -1,114 +1,189 @@
 "use strict"; // директива определяющая что код работает в новом варианте кода 
 //(строгий режим задания переменных)
-const numberOfFilms = +prompt("Сколько фильмов Вы пострели","5");
-//!!! 19 урок
-//alert(numberOfFilms);
+//!!! урок 22 
+// let num = 50;
 
-// const personalMovieDB = {
-//     count: numberOfFilms,
-//     movies:{},
-//     actors:{},
-//     genres:[],
-//     privat: false
-// };
+// // while (num <= 55) {
+// //     console.log(num);
+// //     num++;
+// // }
 
-// let f1 = prompt("Один из последних фильмов?",""),
-//     r1 = prompt("На сколько его офениваете?","1.0");
-// personalMovieDB.movies[f1] = r1;
-// //alert(personalMovieDB.movies[f1]);
-// let f2 = prompt("Один из последних фильмов?",""),
-//     r2 = prompt("На сколько его офениваете?","1.0");
-// personalMovieDB.movies[f2] = r2;
-// let f3 = prompt("Один из последних фильмов?",""),
-//     r3 = prompt("На сколько его офениваете?","1.0");
-// personalMovieDB.movies[f3] = r3;
+// // do {
+// //     console.log(num);
+// //     num++;  
+// // }
+// // while (num <=55);
 
-// console.log(personalMovieDB);
+// for (let i = 1; i < 8; i++){
+//    // console.log(i);  
+    
 
-//!!! 20 урок
-
-// if (4==9) {
-//     console.log('error');
-// }else{
-//     console.log('Ok');
-// };
-
-// const num = 145;
-
-// if (num < 49) {
-//     console.log('error');
-// }else if (num > 100){ 
-//     console.log('error2');
-// }else{
-//     console.log('OK');
-// };
-
-// (num ===145) ? console.log('OK') : console.log('error'); //пряма запись иф елс
-
-// const num = 34;
-// //switch - строгое сравнение
-// switch (num) {
-//     case 49:
-//         console.log('Не верно');
+//     if (i === 6){
 //         break;
-//     case 45:
-//         console.log('OK');
-//         break;
-//     case 100:
-//         console.log('Не верно');
-//         break;
-//     default:
-//         console.log('Ничего не подошло');
-//         break;
-// };
+//     }; 
 
-//!!! 21 урок
+//     if (i === 3){
+//         continue;
+//     }; 
 
-// const hamburger = true;
-// const fries = true;
-
-// if (hamburger && fries){
-// console.log('Ya poel');
-// };
-
-// console.log((hamburger && fries));
-
-// const hamburger = 5;
-// const fries = 0;
-
-// if (hamburger && fries){
-// console.log('Ya poel');
-// }else{
-//     console.log('Ne poel');
+//     console.log(i );
+//     // num++;
 // }
 
-// console.log((hamburger && fries));
-
-// const hamburger = 4;
-// const fries = 1;
-// const cola = 5;
-// // if (hamburger===3 && fries===1){
-//     // проверка числа на булево - просто налчие значение - тру, 0 - фолс
-// if (hamburger >=3 && fries && cola){
-//     console.log('Ya poel');
-// }else{
-//     console.log('Ne poel');
-// }
-// console.log((hamburger >=3 && fries && cola)); // вертает первое ложное значение
-// или последнее трушное
-// console.log((hamburger >=3 && '' && cola));
-
-const hamburger = 4;
-const fries = 1;
-const cola = 0;
-const nuggets = 2;
-
-if (hamburger >=3 && !cola || fries >1 && nuggets){
-    console.log('Ya poel');
-}else{
-    console.log('Ne poel');
+//!!! урок 23
+for(let i = 0; i <3; i++){
+    console.log(i);
+    for(let j = 5; j <7; j++){
+        console.log(j);
+    }
 }
 
-let jonReport, samReport, marinaReport = 'done';
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+// *******
+// let text = "";
+// const leght = 19;
+// for(let i = 0; i <leght; i++){
+//     text = text+'*';
+//     console.log(text);  
+// }
 
-console.log(jonReport || samReport || marinaReport);
+// let text = "";
+// let res = "";
+// const leght = 19;
+// for(let i = 0; i <leght; i++){
+//     text = text+'*';
+//     res = res+text+'\n';     
+// }
+
+let res = "";
+const leght = 19;
+for(let i = 0; i <leght; i++){
+    for (let j=0;j<i; j++){
+        res +='*';
+    }    
+    res += '\n';     
+}
+console.log(res); 
+
+const leght = 5;
+// first: - метка, можно к ней обращатся в циклах для понимания 
+// какой цикл остановить или пропустить
+first: for(let i = 0; i <leght; i++){
+    console.log(`1 level: ${i}`); 
+    for (let j=0;j<i; j++){
+        console.log(`2 level: ${j}`); 
+        for (let k=0;k<j; k++){
+            if (k === 2) continue first;
+            console.log(`3 level: ${k}`); 
+        }   
+    }    
+}
+
+// !! практика 1
+for (let i = 1; i <=10; i++){
+    i++;
+    console.log(i);
+}
+
+for (let i = 2; i <= 16; i++) {
+    if (i % 2 === 0) {
+        continue;
+    } else {
+          console.log(i);
+    }
+}
+    
+
+let num = 2;
+while (num < 16){
+    num++;
+    if (num % 2 === 0) {
+        continue;
+    } else {
+        console.log(num);
+    }
+    
+}
+
+const arrayOfNumbers = [];
+let iter = 0;
+for (let i = 5; i <=10; i++){
+    arrayOfNumbers[iter] = i;
+    iter++;
+}
+
+console.log(arrayOfNumbers);
+// !! практика 2
+const arr = [3, 5, 8, 16, 20, 23, 50];
+const result = [];
+
+for (let i = 0; i <arr.length; i++){
+    result[i] = arr[i];    
+}
+
+console.log(result);
+
+const data = [5, 10, 'Shopping', 20, 'Homework'];
+const result = [];
+
+let iter = 0;
+for (let i = (data.length-1); i >=0 ; i--){
+    result[iter] = data[i]; 
+    iter++;
+}
+
+console.log(result);
+
+const lines = 5;
+let result = '';
+
+// 11 символов в строке
+const allSymbols = (lines*2);
+let symbols = allSymbols;
+const arr = [];
+let iter = 0;
+let res = '';
+let symb = 0;
+for (let i = 0; i <= lines ; i++){
+    res = '';
+    for (let j=0;j<symb;j++){
+        res +=' ';    
+    }
+    for (let j=0;j<=symbols;j++){
+        res +='*';    
+    }
+
+    // for (let j=0;j<symb;j++){
+    //     res +=' ';    
+    // }
+    symbols = symbols-2;
+    symb = (allSymbols-symbols)/2;
+    arr[iter] = res;
+    iter++;
+}
+
+
+for (let i = (arr.length-1); i >= 0; i--){
+   result += arr[i]+'\n';
+}
+
+console.log(result);
+
+const lines = 5;
+let result = '';
+for (let i = 0; i <= lines; i++) {
+    for (let j = 0; j < lines - i; j++) {
+        result += " ";
+    }
+    for (let j = 0; j < 2 * i + 1; j++) {
+        result += "*";
+    }
+    result += "\n";
+}
+
+console.log(result)
