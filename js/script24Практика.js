211d23982348d1942e5270e19e84ce9e863e1995
Maks <moskalenko.maksim1990@gmail.com>
2022-04-17 12:19:16 +0300

    24-25

--- js/script24Практика.js
+++ js/script24Практика.js
@@ -1,37 +1,71 @@
 "use strict"; // директива определяющая что код работает в новом варианте кода 
 //(строгий режим задания переменных)
 //!!! урок 24 
-const numberOfFilms = +prompt("Сколько фильмов Вы пострели","5");
-if (numberOfFilms < 10) {
-   alert('просмотрено довольно мало фильмов'); 
-}else{
-    if (numberOfFilms >= 10 && numberOfFilms < 30) {
-        alert('Вы класический зритель'); 
-     }else{
-        alert('Вы киноман');   
-     }
+// const numberOfFilms = +prompt("Сколько фильмов Вы пострели","5");
+// if (numberOfFilms < 10) {
+//    alert('просмотрено довольно мало фильмов'); 
+// }else{
+//     if (numberOfFilms >= 10 && numberOfFilms < 30) {
+//         alert('Вы класический зритель'); 
+//      }else{
+//         alert('Вы киноман');   
+//      }
+// }
+
+// const personalMovieDB = {
+//          count: numberOfFilms,
+//         movies:{},
+//         actors:{},
+//         genres:[],
+//         privat: false
+//  };
+    
+// let f='',
+//     r = '';
+//  for (let i =0;i<3;i++){
+
+//     while (f==='' || f.length >50 || f===null) {
+//         f = prompt("Один из последних фильмов?","");
+//         if (f !=null && f.length > 0 && f.length <50) {
+//             r = prompt("На сколько его офениваете?","1.0");
+//         }
+//     }
+//     personalMovieDB.movies[f] = r;
+//     f = '';
+//  }
+ 
+//  console.log(personalMovieDB.movies);
+
+//!!! урок 25
+let num = 20;
+function showFirstMessage(text){
+   console.log(text);
+   num = 10; 
 }
 
-const personalMovieDB = {
-         count: numberOfFilms,
-        movies:{},
-        actors:{},
-        genres:[],
-        privat: false
- };
-    
-let f='',
-    r = '';
- for (let i =0;i<3;i++){
+showFirstMessage("Hello! First message");
+console.log(num);
 
-    while (f==='' || f.length >50 || f===null) {
-        f = prompt("Один из последних фильмов?","");
-        if (f !=null && f.length > 0 && f.length <50) {
-            r = prompt("На сколько его офениваете?","1.0");
-        }
-    }
-    personalMovieDB.movies[f] = r;
-    f = '';
- }
- 
- console.log(personalMovieDB.movies);
+function calc(a,b){
+   return (a+b);
+}
+
+console.log(calc(2,5));
+console.log(calc(1,4));
+
+function ret(){
+   let num = 6;
+   return num;
+}
+
+const anotherNum = ret();
+
+const logger = function(){
+   console.log("Hello!");
+};
+
+logger();
+
+const calc2 = (a,b) => {return a+b};
+
+console.log(calc2(1,5));
