c84ab8ad2d824205c4ba65f0d18ea2ed77e2f232
Maks <moskalenko.maksim1990@gmail.com>
2022-05-11 17:22:41 +0300

    les 116

---
+++ js/script116.js
@@ -0,0 +1,16 @@
+'use strict';
+// урок 116
+// готовые решения 
+// https://github.com/ganlanyuan/tiny-slider
+import { tns } from "./node_modules/tiny-slider/src/tiny-slider";
+
+tns({
+  container: '.my-slider2',
+  items: 1,
+  slideBy: 'page',
+  autoplay: true
+});
+
+
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ let path = require('path');
 
 module.exports = {
   mode: 'production',
-  entry: './js/script108.js',
+  entry: './js/script116.js',
   output: {
     filename: 'bundle.js',
     path: __dirname + '/js'
